f6ceccdab007042fe2b9eaaed6ef07281560b91e
ana.lopes <ana.lopes011@edu.pucrs.br>
2025-03-24 21:28:59 -0300

    ex2

--- DesafioPadroesProjeto-2025-main/Q2_LogSimples/src/App.java
+++ DesafioPadroesProjeto-2025-main/Q2_LogSimples/src/App.java
@@ -1,12 +1,20 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        LogSimples log = new LogSimples();
+    public static void main(String[] args) {
+        LogSingleton log = LogSingleton.getInstancia();
         log.log("Mensagem de alerta 1");
         log.log("Mensagem de alarme 23");
         log.log("Mensagem de PANICO!!");
 
-        for(String m:log){
-            System.out.println(m);
+        for (String mensagem : log) {
+            System.out.println(mensagem);
+        }
+
+        LogSingleton logS = LogSingleton.getInstancia();
+        logS.log("Nova mensagem de alerta");
+        
+        System.out.println("logs");
+        for (String mensagem : log) {
+            System.out.println(mensagem);
         }
     }
 }
