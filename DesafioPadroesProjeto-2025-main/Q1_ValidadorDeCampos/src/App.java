9ef8968f218999e3c916f49e9c4f7e8c7cc40ad4
Fernanda Farias <143156498+FeFeFarias05@users.noreply.github.com>
2025-03-30 22:59:31 +0000

    ultimas alteracoes

--- DesafioPadroesProjeto-2025-main/Q1_ValidadorDeCampos/src/App.java
+++ DesafioPadroesProjeto-2025-main/Q1_ValidadorDeCampos/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String email = "bernardo.copstein@pucrs.br";
-        var validador = new Validador();
+        Validador validador = ValidadorFactory.getValidador(ValidadorFactory.Tipo.EMAIL);
 
         if (validador.valida(Validador.Tipo.EMAIL, email)){
             System.out.println(email+" é um email válido!");
